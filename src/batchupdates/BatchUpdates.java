fec1aa540e4df31f5d35bb80b3db5c76e7d6b654
reachg2 <mazeltovclap@gmail.com>
2018-01-11 08:47:58 +0700

    batch update

--- src/batchupdates/BatchUpdates.java
+++ src/batchupdates/BatchUpdates.java
@@ -18,10 +18,17 @@ public class BatchUpdates {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/banking","root","");
             Statement stt = con.createStatement();
+            
+            //create an array of string to store all queries
+            String[] queries = new String[3];
+            queries[0] = "insert into bank values ('5','cheat','10000') ";
+            queries[1] = "insert into bank values ('6','phaneth','1000') ";
+            queries[2] = "update bank set balance = '10000' where acno = 6";
+            
             //add all queries into the batch
-            stt.addBatch("insert into bank values ('3','chhun','10000') ");
-            stt.addBatch("insert into bank values ('4','vad','1000') ");
-            stt.addBatch("update bank set balance = '10000' where acno = 4");
+            for(String query:queries)
+            stt.addBatch(query);
+            
             //execute all the queries in the batch one for all 
             stt.executeBatch();
             //batch is used to execute all the queries one for all instead of execute each queries one by one
